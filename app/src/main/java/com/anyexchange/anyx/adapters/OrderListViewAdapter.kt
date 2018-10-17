75dace24ff22c68cee69349bbe92bf6127747ff8
jmnbehar <jbehar@mytrus.com>
2018-10-16 17:27:19 -0700

    More small improvements for orderList

--- app/src/main/java/com/anyexchange/anyx/adapters/OrderListViewAdapter.kt
+++ app/src/main/java/com/anyexchange/anyx/adapters/OrderListViewAdapter.kt
@@ -5,9 +5,7 @@ import android.content.res.Resources
 import android.support.v4.content.res.ResourcesCompat
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.anyexchange.anyx.classes.*
 import com.anyexchange.anyx.R
 import kotlinx.android.synthetic.main.list_row_order.view.*
@@ -17,7 +15,7 @@ import org.jetbrains.anko.backgroundColor
  * Created by anyexchange on 11/12/2017.
  */
 
-class OrderListViewAdapter(val context: Context, val orders: List<Order>, var resources: Resources, private var orderOnClick: (Order) -> Unit = { }) : BaseAdapter() {
+class OrderListViewAdapter(val context: Context, val orders: List<Order>, var resources: Resources, private var orderOnClick: (Order) -> Unit, private var cancelButtonClicked: (Order) -> Unit) : BaseAdapter() {
 
     override fun getCount(): Int {
         return if (orders.isEmpty()) {
@@ -41,7 +39,14 @@ class OrderListViewAdapter(val context: Context, val orders: List<Order>, var re
         var mainLabelText: TextView? = null
 
         var priceText: TextView? = null
-        var tradeTypeText: TextView? = null
+
+        var extraInfoLayout: LinearLayout? = null
+
+        var timeInForceText: TextView? = null
+        var idDateText: TextView? = null
+        var amountText: TextView? = null
+
+        var cancelButton: Button? = null
     }
 
     override fun getView(i: Int, convertView: View?, viewGroup: ViewGroup): View {
@@ -56,7 +61,13 @@ class OrderListViewAdapter(val context: Context, val orders: List<Order>, var re
             viewHolder.mainLabelText = vi.txt_order_label
 
             viewHolder.priceText = vi.txt_order_price
-            viewHolder.tradeTypeText = vi.txt_order_trade_type
+
+            viewHolder.extraInfoLayout = vi.layout_order_extra_info
+
+            viewHolder.idDateText = vi.txt_order_id_and_date
+            viewHolder.amountText = vi.txt_order_amount
+            viewHolder.timeInForceText = vi.txt_order_time_in_force
+            viewHolder.cancelButton = vi.btn_order_cancel
 
             vi.tag = viewHolder
             outputView = vi
@@ -65,39 +76,30 @@ class OrderListViewAdapter(val context: Context, val orders: List<Order>, var re
             outputView = convertView
         }
 
-        val tradeSide: TradeSide
-        val price: Double
-        val amount: Double
-        val tradeType: TradeType?
-        val currency: Currency
-
         if (orders.isEmpty()) {
             viewHolder.colorView?.visibility = View.INVISIBLE
             viewHolder.mainLabelText?.text = context.resources.getString(R.string.chart_history_no_orders)
             viewHolder.priceText?.visibility = View.GONE
-            viewHolder.tradeTypeText?.visibility = View.GONE
+            viewHolder.extraInfoLayout?.visibility = View.GONE
             return outputView
         } else {
             val order = orders[i]
-            tradeSide = order.side
-            price = order.price
+//            val tradeSide = order.side
+//            val price = order.price
 
             val size = order.amount
             val filled = order.filledAmount
-            val unfilledSize = size - filled
-            amount = unfilledSize
-            currency = order.tradingPair.baseCurrency
-            tradeType = TradeType.forString(order.type)
+            val amount = size - filled
+            val currency = order.tradingPair.baseCurrency
             outputView.setOnClickListener { orderOnClick(order) }
 
 
-            viewHolder.colorView?.backgroundColor = when (tradeSide) {
+            viewHolder.colorView?.backgroundColor = when (order.side) {
                 TradeSide.BUY -> ResourcesCompat.getColor(resources, R.color.anyx_green, null)
                 TradeSide.SELL -> ResourcesCompat.getColor(resources, R.color.anyx_red, null)
             }
 
-
-            val sideString = when (tradeSide) {
+            val sideString = when (order.side) {
                 TradeSide.BUY -> context.resources.getString(R.string.chart_history_order_side_buy)
                 TradeSide.SELL -> context.resources.getString(R.string.chart_history_order_side_sell)
             }
@@ -107,14 +109,26 @@ class OrderListViewAdapter(val context: Context, val orders: List<Order>, var re
 
             viewHolder.mainLabelText?.text = context.resources.getString(R.string.chart_fill_main_label, sideString, amount.format(currency), totalPrice.format(quoteCurrency))
 
-            viewHolder.priceText?.text = price.fiatFormat(Account.defaultFiatCurrency)
+            viewHolder.priceText?.text = order.price.fiatFormat(Account.defaultFiatCurrency)
 
+//            val tradeType = TradeType.forString(order.type)
             val tradeTypeString = order.type.capitalize()
-            viewHolder.priceText?.text = context.resources.getString(R.string.chart_order_price_label, tradeTypeString, price.format(quoteCurrency), currency)
+            viewHolder.priceText?.text = context.resources.getString(R.string.chart_order_price_label, tradeTypeString, order.price.format(quoteCurrency), currency)
 
+            if (order.showExtraInfo) {
+                viewHolder.extraInfoLayout?.visibility = View.VISIBLE
 
-            viewHolder.tradeTypeText?.visibility = View.VISIBLE
-            viewHolder.tradeTypeText?.text = context.resources.getString(R.string.chart_history_trade_type_label, tradeType)
+                //TODO: format this better:
+                viewHolder.idDateText?.text = order.id + " " + order.time.format(Fill.dateFormat)
+                viewHolder.amountText?.text = "Full Amount: ${order.amount}, Filled Amount: ${order.filledAmount}, remaining: $amount"
+                viewHolder.timeInForceText?.text = order.timeInForce.capitalize()
+
+                viewHolder.cancelButton?.setOnClickListener {
+                    cancelButtonClicked(order)
+                }
+            } else {
+                viewHolder.extraInfoLayout?.visibility = View.GONE
+            }
 
             return outputView
         }
