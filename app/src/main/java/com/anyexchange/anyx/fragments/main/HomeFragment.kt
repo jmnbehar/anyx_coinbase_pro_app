46074689dac6a0c331d557d752292c8dcf5e6a62
jmnbehar <jbehar@mytrus.com>
2018-12-24 14:59:52 -0800

    begins to split up homeFragment

--- app/src/main/java/com/anyexchange/anyx/fragments/main/HomeFragment.kt
+++ app/src/main/java/com/anyexchange/anyx/fragments/main/HomeFragment.kt
@@ -8,6 +8,11 @@ import android.view.ViewGroup
 import com.anyexchange.anyx.adapters.HomePagerAdapter
 import com.anyexchange.anyx.classes.*
 import com.anyexchange.anyx.R
+import com.anyexchange.anyx.adapters.ProductListViewAdapter
+import com.anyexchange.anyx.classes.api.AnyApi
+import com.anyexchange.anyx.classes.api.CBProApi
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.result.Result
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 /**
@@ -51,6 +56,78 @@ class HomeFragment : RefreshFragment() {
 
         return rootView
     }
+
+    override fun refresh(onComplete: (Boolean) -> Unit) {
+        skipNextRefresh = true
+
+        val onFailure: (result: Result.Failure<String, FuelError>) -> Unit = { result ->
+            toast("Error!: ${result.errorMessage}")
+            onComplete(false)
+        }
+
+        ///TODO: run all 3 sub-refreshes, preferably run the active page's first
+        /// after completing the first page run the other 2, only hide the spinner once all 3 are completed
+
+        super.refresh(onComplete)
+
+    }
+
+    fun marketRefresh(onFailure: (result: Result.Failure<String, FuelError>) -> Unit, onComplete: (Boolean) -> Unit) {
+        AnyApi(apiInitData).updateAllTickers(onFailure) {
+            //Complete Market Refresh
+        }
+    }
+
+    fun accountsRefresh(onFailure: (result: Result.Failure<String, FuelError>) -> Unit, onComplete: (Boolean) -> Unit) {
+        val context = context
+        if (context != null && Prefs(context).isLoggedIn) {
+            CBProApi.accounts(apiInitData).updateAllAccounts(onFailure) {
+                //Complete accounts refresh
+            }
+        } else {
+            onComplete(true)
+        }
+    }
+    fun favoritesRefresh(onFailure: (result: Result.Failure<String, FuelError>) -> Unit, onComplete: (Boolean) -> Unit) {
+        val onFailure: (result: Result.Failure<String, FuelError>) -> Unit = { result -> toast("Error!: ${result.errorMessage}") }
+        var productsUpdated = 0
+        val time = Timespan.DAY
+        val favoriteProducts = Product.favorites()
+        val count = favoriteProducts.count()
+        for (product in favoriteProducts) {
+            //always check multiple exchanges?
+            product.defaultTradingPair?.let { tradingPair ->
+                product.updateCandles(time, tradingPair, apiInitData, {
+                    //OnFailure
+                }) { didUpdate ->
+                    //OnSuccess
+                    if (lifecycle.isCreatedOrResumed) {
+                        if (didUpdate) {
+                            productsUpdated++
+                            if (productsUpdated == count) {
+                                context?.let {
+                                    Prefs(it).stashedProducts = Product.map.values.toList()
+                                }
+                                //update Favorites Tab
+                                onComplete(true)
+                            }
+                        } else {
+                            AnyApi(apiInitData).ticker(tradingPair, onFailure) {
+                                productsUpdated++
+                                if (productsUpdated == count) {
+                                    //update Favorites Tab
+                                    onComplete(true)
+                                }
+                            }
+                        }
+                    }
+                }
+            } ?: run {
+                onFailure(Result.Failure(FuelError(Exception())))
+            }
+        }
+    }
+
 
     override fun onPause() {
         handler.removeCallbacks(autoRefresh)
